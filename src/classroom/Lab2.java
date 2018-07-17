4aca04c11fb95e53c35e50cdd517c2d6d39e8597
Amna Anjum <amnaanjum@Amnas-MacBook-Air.local>
2018-07-17 11:53:54 -0400

    Initial Commit

---
+++ src/classroom/Lab2.java
@@ -0,0 +1,35 @@
+package classroom;
+
+import java.util.Scanner;
+
+public class Lab2 {
+	do {
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner scnr = new Scanner(System.in);
+			
+			System.out.println("Enter value of length in feet.");
+			double length = scnr.nextDouble();
+			
+			System.out.println("Enter value of width in feet.");
+			double width = scnr.nextDouble();
+			
+			System.out.println("Enter vaue of height");
+			double height = scnr.nextDouble(); 
+			
+			double Perimeter = width + width + length + length;
+			double Area = width * length; 
+			double Volume = length * width * height;
+					
+			System.out.println("Perimeter of classroom is " + Perimeter + " feet.");
+			System.out.println("Area of the classroom is " + Area + " feet squared.");
+			System.out.println("Volume of the classroom is " + Volume + " feet cubed");
+			
+			scnr.close();
+			
+			System.out.println("Would you like to continue? (Yes/No)");
+	 
+	while (userInput == 'Yes');
+		}
+	}
+}
